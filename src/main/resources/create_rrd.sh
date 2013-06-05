ba9e1cc19167b42a22af236fdaa68b36692e57e0
Arun Horne <arun@arunhorne.co.uk>
2013-06-05 22:06:25 +0100

    Alpha release

--- src/main/resources/create_rrd.sh
+++ src/main/resources/create_rrd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rrdtool create /tmp/current_cost.rrd --step 5 \
+rrdtool create /opt/current-cost/db/current-cost.rrd --step 5 \
 DS:Power:GAUGE:180:0:U \
 DS:Temperature:GAUGE:180:U:U \
 RRA:AVERAGE:0.5:1:3200 \
@@ -27,3 +27,4 @@ RRA:MAX:0.5:1008:3200 \
 RRA:MAX:0.5:4320:3200 \
 RRA:MAX:0.5:52560:3200 \
 RRA:MAX:0.5:525600:3200
+
--- src/main/resources/generate_graphs.sh
+++ src/main/resources/generate_graphs.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-rrdtool graph /tmp/power-10min.png \
+CC_DIR=/opt/current-cost
+DB_FILE=$CC_DIR/db/current-cost.rrd
+IMG_DIR=$CC_DIR/graphs
+
+rrdtool graph $IMG_DIR/cc-10min.png \
 --start end-10m --width 700 --end now --slope-mode \
 --no-legend --vertical-label Watts --lower-limit 0 \
 --alt-autoscale-max \
-DEF:Power=/tmp/current_cost.rrd:Power:AVERAGE \
+DEF:Power=$DB_FILE:Power:AVERAGE \
 LINE3:Power#0000FF:"Average"
 
-rrdtool graph /tmp/power-day.png \
+rrdtool graph $IMG_DIR/cc-1day.png \
 --start end-1d --width 700 --end now --slope-mode \
 --no-legend --vertical-label Watts --lower-limit 0 \
 --alt-autoscale-max \
-DEF:Power=/tmp/current_cost.rrd:Power:AVERAGE \
-DEF:PowerMin=/tmp/current_cost.rrd:Power:MIN \
-DEF:PowerMax=/tmp/current_cost.rrd:Power:MAX \
+DEF:Power=$DB_FILE:Power:AVERAGE \
+DEF:PowerMin=$DB_FILE:Power:MIN \
+DEF:PowerMax=$DB_FILE:Power:MAX \
 CDEF:PowerRange=PowerMax,PowerMin,- \
 LINE1:PowerMin: \
 AREA:PowerRange#0000FF11:"Error Range":STACK \
@@ -21,16 +25,17 @@ LINE1:PowerMin#0000FF33:"Min" \
 LINE1:PowerMax#0000FF33:"Max" \
 LINE1:Power#0000FF:"Average"
 
-rrdtool graph /tmp/power-week.png \
+rrdtool graph $IMG_DIR/cc-7day.png \
 --start end-7d --width 700 --end now --slope-mode \
 --no-legend --vertical-label Watts --lower-limit 0 \
 --alt-autoscale-max \
-DEF:Power=/tmp/current_cost.rrd:Power:AVERAGE \
-DEF:PowerMin=/tmp/current_cost.rrd:Power:MIN \
-DEF:PowerMax=/tmp/current_cost.rrd:Power:MAX \
+DEF:Power=$DB_FILE:Power:AVERAGE \
+DEF:PowerMin=$DB_FILE:Power:MIN \
+DEF:PowerMax=$DB_FILE:Power:MAX \
 CDEF:PowerRange=PowerMax,PowerMin,- \
 LINE1:PowerMin: \
 AREA:PowerRange#0000FF11:"Error Range":STACK \
 LINE1:PowerMin#0000FF33:"Min" \
 LINE1:PowerMax#0000FF33:"Max" \
 LINE1:Power#0000FF:"Average"
+
